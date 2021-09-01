dd60cac22b91832e147ce4e2510b641f624d83d4
Constantin Ungureanu <constantin.ungureanu.github@gmail.com>
2021-09-01 16:02:14 +0200

    Add firebase support.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { doc, getFirestore } from 'firebase/firestore';
+import { FirebaseAppProvider, FirestoreProvider, useFirestore, useFirebaseApp, useFirestoreDocData } from 'reactfire';
+import { firebaseConfig } from './firebase';
+import './App.scss';
+
+function BurritoTaste() {
+  const firestore = useFirestore();
+  const burritoRef = doc(firestore, 'tryreactfire', 'burrito');
+  const { status, data } = useFirestoreDocData(burritoRef);
+
+  if (status === 'loading') {
+    return <p>Fetching burrito flavor...</p>;
+  }
+
+  return <p>The burrito is {data.yummy ? 'good' : 'bad'}!</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <FirestoreProvider sdk={getFirestore(useFirebaseApp())}>
+        <h1>🌯</h1>
+        <BurritoTaste />
+      </FirestoreProvider>
+    </FirebaseAppProvider>
+  )
 }
 
 export default App;
